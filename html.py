9ff7660da71f5a159e686eb734f992ef96f75d41
John Boyle <boylejm@gmail.com>
2014-09-19 05:15:03 -0400

    add hewr banner png to html.py

--- html.py
+++ html.py
@@ -28,7 +28,7 @@ BANNER_IMAGES = \
       'foee': [ 'banner_zin.png', 'FoEE' ],
       'grar': [ 'banner_zin.png', 'GrAr' ],
       'ddbe': [ 'banner_ddbe.png', 'DDBe' ],
-      'hewr': [ 'banner_zin.png', 'HEWr' ],
+      'hewr': [ 'banner_hewr.png', 'HEWr' ],
       'opvm': [ 'banner_zin.png', 'OpVM' ],
       'tecj': [ 'banner_zin.png', 'TeCj' ],
       'vsmo': [ 'banner_zin.png', 'VSMo' ],
